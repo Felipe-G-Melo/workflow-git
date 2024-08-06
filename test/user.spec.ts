d5b7dca18bae72546b0455ca9b4846e8eb141e2e
Felipe Melo <melolipedev@gmail.com>
2024-08-06 19:41:17 -0300

    first commit

---
+++ test/user.spec.ts
@@ -0,0 +1,9 @@
+import { describe, expect, test } from '@jest/globals';
+import { User } from '../src/user';
+
+describe('deve retornar true', () => {
+  test('deve retornar true', () => {
+    var user = new User();
+    expect(user.returnTrue()).toBe(false);
+  });
+});
