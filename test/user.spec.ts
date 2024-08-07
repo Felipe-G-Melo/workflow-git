acab13fef22af916175bba930d4bb70c1b42b25e
Felipe Melo <melolipedev@gmail.com>
2024-08-07 17:28:14 -0300

    a

--- test/user.spec.ts
+++ test/user.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test } from '@jest/globals';
 import { User } from '../src/user';
 
-describe('deve retornar true', () => {
+describe('deve retornar rue', () => {
   test('deve retornar true', () => {
     var user = new User();
     expect(user.returnTrue()).toBe(false);
